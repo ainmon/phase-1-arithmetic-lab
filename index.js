2a771da1f4d2e4568b396072d70328b7040afb68
Adam <inmon.adam@yahoo.com>
2021-09-21 20:37:35 -0500

    commit tree 75fe5f3d08e45955a4b632c5e699182d1a15efa3

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+var num1 = 31;
 
-// Write your code here
+var num2 = 2;
+
+var num3 = 16;
+
+var num4 = 12;
+
+var num5 = 20;
+
+var multiply = (num1 * num2);
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+var mod = (num3 % num4);
+
+var max = Math.max(num2, num3, num4, num5);
